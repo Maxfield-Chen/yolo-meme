b4500e673e6b347643b97f238b56d2409bd46c78
modulo <maxfieldchen@gmail.com>
2014-08-20 14:48:15 -0400

    Added the serial port code, i'm not so sure about the com1 path, needs review

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 
+//serving pages
 app.get("/", function(req,res){
   res.sendfile('./index.html');
 });
@@ -9,6 +10,25 @@ app.post("/user/add", function(req,res){
   res.send("OK");
 });
 
+//Rfid response code
+var SerialPort = require("serialport").SerialPort
+//I'm not so sure about this com1 business, would appreciate review
+var serialPort = new SerialPort("/com1", {
+  baudrate: 9600
+});
+
+serialPort.on("open", function () {
+  console.log('open');
+  serialPort.on('data', function(data) {
+    console.log('data received: ' + data);
+  });
+  serialPort.write("ls\n", function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });
+});
+
+//Final Server Initialization 
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log("Node server running on port: " + port);
